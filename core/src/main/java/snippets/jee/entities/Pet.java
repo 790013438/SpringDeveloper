0e6b2579fafb8910aa89180447a91cd9c0ed7136
790013438 <790013438@qq.com>
2017-11-05 16:07:28 +0800

    完善field

--- core/src/main/java/snippets/jee/entities/Pet.java
+++ core/src/main/java/snippets/jee/entities/Pet.java
@@ -3,4 +3,10 @@ package snippets.jee.entities;
 import snippets.jee.base.AbstractEntity;
 
 public class Pet extends AbstractEntity {
+    private User owner;
+    private snippets.jee.base.PetType petType;
+    private String name;
+    private Integer age;
+    private String details;
+    private String rfid;
 }
--- core/src/main/java/snippets/jee/entities/Response.java
+++ core/src/main/java/snippets/jee/entities/Response.java
@@ -3,4 +3,8 @@ package snippets.jee.entities;
 import snippets.jee.base.AbstractEntity;
 
 public class Response extends AbstractEntity {
+    private User user;
+    private Request request;
+    private snippets.jee.base.ResponseStatus responseStatus;
+    private String details;
 }
