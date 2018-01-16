3c6d8cbf04d7d6cf999fc74daacb7afecc8c7c18
790013438 <790013438@users.noreply.github.com>
2018-01-16 16:04:39 +0800

    spring-boot网页示例
    从后端到前端

---
+++ spring-boot-journal/src/main/java/com/example/domain/Journal.java
@@ -0,0 +1,92 @@
+package com.example.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Entity
+public class Journal {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private Date created;
+    private String summary;
+
+    @Transient
+    private SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+
+    public Journal(String title, String sumary, String date) throws ParseException {
+        this.title = title;
+        this.summary = summary;
+        this.created = format.parse(date);
+    }
+
+    Journal() {}
+
+    @Override
+    public String toString() {
+        StringBuilder value = new StringBuilder("JournalEntry(");
+        value.append("Id: ");
+        value.append(id);
+        value.append(",Title: ");
+        value.append(title);
+        value.append(",Summary: ");
+        value.append(summary);
+        value.append(",Created: ");
+        value.append(getCreatedAsShort());
+        value.append(")");
+        return value.toString();
+    }
+
+    public String getCreatedAsShort() {
+        return format.format(created);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public SimpleDateFormat getFormat() {
+        return format;
+    }
+
+    public void setFormat(SimpleDateFormat format) {
+        this.format = format;
+    }
+}
