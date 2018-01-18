50125edc21f5328254e6c49b727ce1bfa27f8a89
790013438 <790013438@users.noreply.github.com>
2018-01-18 13:56:31 +0800

    添加排除的代码

--- Demo/src/main/java/com/example/Demo/DemoApplication.java
+++ Demo/src/main/java/com/example/Demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.example.Demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={ActiveMQAutoConfiguration.class,DataSourceAutoConfiguration.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
