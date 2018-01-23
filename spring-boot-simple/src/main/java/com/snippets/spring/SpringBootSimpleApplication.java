ca8af22138695627977a90fcb5e64ee41c6ea766
790013438 <790013438@users.noreply.github.com>
2018-01-23 10:24:16 +0800

    用SpringApplicationBuilder(),分层的代码
    ```java
    new SpringApplicationBuilder()
            .bannerMode(Banner.Mode.OFF)
            .sources(SpringBootSimpleApplication.class)
            .run(args);
    ```

--- spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
+++ spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
@@ -1,19 +1,17 @@
 package com.snippets.spring;
 
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-
-import java.io.PrintStream;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringBootSimpleApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication app = new SpringApplication(SpringBootSimpleApplication.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
+        new SpringApplicationBuilder()
+                .bannerMode(Banner.Mode.OFF)
+                .sources(SpringBootSimpleApplication.class)
+                .run(args);
     }
 }
