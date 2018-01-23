474d6afe3bd66d4229eba8c8dcac5075056cd679
790013438 <790013438@users.noreply.github.com>
2018-01-23 09:34:33 +0800

    banner

--- spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
+++ spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
@@ -1,12 +1,24 @@
 package com.snippets.spring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class SpringBootSimpleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootSimpleApplication.class, args);
+
+        SpringApplication app = new SpringApplication(SpringBootSimpleApplication.class);
+        app.setBanner(new Banner() {
+            @Override
+            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+                out.println("\n\n\tThis is my own banner!\n\n".toUpperCase());
+            }
+        });
+        app.run(args);
     }
 }
