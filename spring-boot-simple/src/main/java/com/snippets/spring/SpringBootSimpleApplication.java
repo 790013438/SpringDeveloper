0eb3a12b36fb5d72ee8b3aef73939edf8ddefe65
790013438 <790013438@users.noreply.github.com>
2018-01-25 17:44:13 +0800

    ApplicationEvent events
    ```java
    Logger log = LoggerFactory.getLogger(SpringBootSimpleApplication.class);
    new SpringApplicationBuilder(SpringBootSimpleApplication.class)
            .listeners(new ApplicationListener<ApplicationEvent>() {
    
                @Override
                public void onApplicationEvent(ApplicationEvent event) {
                    log.info("#### > " + event.getClass().getCanonicalName());
                }
            })
            .run(args);
    ```

--- spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
+++ spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
@@ -1,15 +1,26 @@
 package com.snippets.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
 public class SpringBootSimpleApplication {
 
     public static void main(String[] args) {
 
+        Logger log = LoggerFactory.getLogger(SpringBootSimpleApplication.class);
         new SpringApplicationBuilder(SpringBootSimpleApplication.class)
-                .profiles("prod", "cloud")
+                .listeners(new ApplicationListener<ApplicationEvent>() {
+
+                    @Override
+                    public void onApplicationEvent(ApplicationEvent event) {
+                        log.info("#### > " + event.getClass().getCanonicalName());
+                    }
+                })
                 .run(args);
     }
 }
