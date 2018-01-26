3da9c25bd916fbf6d9002f748f10dcab687801e6
790013438 <790013438@users.noreply.github.com>
2018-01-26 14:14:55 +0800

    可以用以前没见过的方式，写bean
    ```java
    @Bean
    String info() {
        return "Just a simple String bean";
    }
    
    @Autowired
    private String info;
    ```
    
    传参数，CommandLineRunner, ApplicationRunner
    
    ```java
    java -jar target/spring-boot-simple-0.0.1-SNAPSHOT.jar --enable arg1 arg2
    ```

--- spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
+++ spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
@@ -4,14 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @SpringBootApplication
-public class SpringBootSimpleApplication {
+public class SpringBootSimpleApplication implements CommandLineRunner, ApplicationRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootSimpleApplication.class);
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(SpringBootSimpleApplication.class)
@@ -19,21 +24,43 @@ public class SpringBootSimpleApplication {
                 .run(args);
     }
 
+    @Bean
+    String info() {
+        return "Just a simple String bean";
+    }
+
+    @Autowired
+    private String info;
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) {
+        LOGGER.info("## > ApplicationRunner Implementation...");
+        LOGGER.info("Accessing the Info bean: " + info);
+        applicationArguments.getNonOptionArgs().forEach(LOGGER::info);
+    }
+
+    @Override
+    public void run(String... args) {
+        LOGGER.info("## > CommandLineRunner Implementation...");
+        LOGGER.info("Accessing the Info bean: " + info);
+        for (String arg : args)
+            LOGGER.info(arg);
+    }
 }
 
 @Component
 class MyComponent {
-    private static final Logger log = LoggerFactory.getLogger(MyComponent.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyComponent.class);
 
     @Autowired
     public MyComponent(ApplicationArguments applicationArguments) {
         boolean enable = applicationArguments.containsOption("enable");
         if (enable)
-            log.info("## > You are enable!");
+            LOGGER.info("## > You are enable!");
 
         List<String> _args = applicationArguments.getNonOptionArgs();
-        log.info("## > extra args ...");
+        LOGGER.info("## > extra args ...");
         if (!_args.isEmpty())
-            _args.forEach(log::info);
+            _args.forEach(LOGGER::info);
     }
 }
