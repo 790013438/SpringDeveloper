cf8fbc05e91dd948acea187441e43acd567e0b6f
790013438 <790013438@users.noreply.github.com>
2018-01-26 11:37:06 +0800

    传参数，Application Arguments
    ```java
    mvnw spring-boot:run -Drun.arguments="--enable"
    ```
    ```java
    mvnw spring-boot:run -Drun.arguments="arg1,arg2"
    ```
    
    Accessing Arguments with an Executable JAR
    ```java
    ./mvnw package
    java -jar target/spring-boot-simple-0.0.1-SNAPSHOT.jar
    java -jar target/spring-boot-simple-0.0.1-SNAPSHOT.jar --enable arg1 arg2
    ```

--- spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
+++ spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
@@ -1,15 +1,39 @@
 package com.snippets.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootSimpleApplication {
 
     public static void main(String[] args) {
-
         new SpringApplicationBuilder(SpringBootSimpleApplication.class)
-                .web(false)
+                .logStartupInfo(false)
                 .run(args);
+    }
+
+}
+
+@Component
+class MyComponent {
+    private static final Logger log = LoggerFactory.getLogger(MyComponent.class);
+
+    @Autowired
+    public MyComponent(ApplicationArguments applicationArguments) {
+        boolean enable = applicationArguments.containsOption("enable");
+        if (enable)
+            log.info("## > You are enable!");
+
+        List<String> _args = applicationArguments.getNonOptionArgs();
+        log.info("## > extra args ...");
+        if (!_args.isEmpty())
+            _args.forEach(log::info);
     }
 }
