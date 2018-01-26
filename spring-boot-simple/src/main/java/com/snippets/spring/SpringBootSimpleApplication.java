e6ace42cffdb7b4a05b1249ad8a8af9d30ec7f85
790013438 <790013438@users.noreply.github.com>
2018-01-26 10:51:35 +0800

    set the web(false) and it’s the same idea as the exclude parameter
    ```java
    new SpringApplicationBuilder(SpringBootSimpleApplication.class)
            .web(false)
            .run(args);
    ```

--- spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
+++ spring-boot-simple/src/main/java/com/snippets/spring/SpringBootSimpleApplication.java
@@ -1,20 +1,15 @@
 package com.snippets.spring;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
 public class SpringBootSimpleApplication {
 
     public static void main(String[] args) {
 
-        Logger log = LoggerFactory.getLogger(SpringBootSimpleApplication.class);
         new SpringApplicationBuilder(SpringBootSimpleApplication.class)
-                .listeners((ApplicationListener<ApplicationEvent>) event -> log.info("#### > " + event.getClass().getCanonicalName()))
+                .web(false)
                 .run(args);
     }
 }
