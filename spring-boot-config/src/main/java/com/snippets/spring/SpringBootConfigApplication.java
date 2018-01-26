0d4ac3d2527c5008e937a39b739daab30a27fb19
790013438 <790013438@users.noreply.github.com>
2018-01-26 17:11:22 +0800

    配置文件，从命令行输入
    直接用maven启动
    ```java
    mvnw spring-boot:run -Dserver.ip=192.168.12.1
    ```
    
    使用java 运行jar 程序
    ```bash
    ./mvnw package -DskipTests=true
    java -jar target/spring-boot-config-0.0.1-SNAPSHOT.jar --server.ip=192.168.12.1
    ```

---
+++ spring-boot-config/src/main/java/com/snippets/spring/SpringBootConfigApplication.java
@@ -0,0 +1,30 @@
+package com.snippets.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootConfigApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringBootConfigApplication.class);
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SpringBootConfigApplication.class)
+                .logStartupInfo(false)
+                .run(args);
+    }
+
+    @Value("${server.ip}")
+    private
+    String serverIp;
+
+    @Bean
+    CommandLineRunner values() {
+        return args -> logger.info(" > The Server IP is:" + serverIp);
+    }
+}
