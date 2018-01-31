99dcfb015a9b6a3657d36d904706d8e48a10753a
790013438 <790013438@users.noreply.github.com>
2018-01-31 17:37:03 +0800

    j2ee方式
    ```java
    mvn archetype:generate -DgroupId=com.snippets.j2ee -DartifactId=simple-web-app -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
    ```
    
    ```java
    mvn -Dmaven.tomcat.port=80 -Dmaven.tomcat.path=/ tomcat7:run
    ```
    
    ```pom.xml
    <build>
        <finalName>wmiig</finalName>
        <plugins>
            <plugin>
                <groupId>org.apache.tomcat.maven</groupId>
                <artifactId>tomcat7-maven-plugin</artifactId>
                <version>2.2</version>
            </plugin>
        </plugins>
    </build>
    ```

---
+++ simple-web-app/src/main/java/com/snippets/j2ee/SimpleServlet.java
@@ -0,0 +1,22 @@
+package com.snippets.j2ee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Simple Web Application with Java</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
