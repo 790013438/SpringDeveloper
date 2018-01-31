233389adecf101095d73f0aed8201e79cb1e06a0
790013438 <790013438@users.noreply.github.com>
2018-01-31 13:55:36 +0800

    设置自己的配置前缀名字
    在src/main/resources 下添加application.properties
    ```java
    server.ip=192.168.3.5
    
    myapp.server-ip=192.168.34.56
    myapp.name=My Config App
    myapp.description=This is an example
    ```
    
    更改端口
    ```java
    spring run app.groovy -- --server.port=8888
    ```

--- spring-boot-config/src/main/java/com/snippets/spring/SpringBootConfigApplication.java
+++ spring-boot-config/src/main/java/com/snippets/spring/SpringBootConfigApplication.java
@@ -2,11 +2,14 @@ package com.snippets.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringBootConfigApplication {
@@ -19,12 +22,50 @@ public class SpringBootConfigApplication {
                 .run(args);
     }
 
-    @Value("${server.ip}")
-    private
-    String serverIp;
+    @Value("${myapp.server-ip}")
+    private String serverIp;
+
+    @Autowired
+    private MyAppProperties myAppProperties;
 
     @Bean
     CommandLineRunner values() {
-        return args -> logger.info(" > The Server IP is:" + serverIp);
+        return args -> {
+            logger.info(" > The Server IP is:" + serverIp);
+            logger.info(" > App Name: " + myAppProperties.getName());
+            logger.info(" > App Info: " + myAppProperties.getDescription());
+        };
+    }
+
+    @Component
+    @ConfigurationProperties(prefix = "myapp")
+    private static class MyAppProperties {
+        private String name;
+        private String description;
+        private String serverIP;
+
+        String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getServerIP() {
+            return serverIP;
+        }
+
+        public void setServerIP(String serverIP) {
+            this.serverIP = serverIP;
+        }
     }
 }
