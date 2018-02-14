51246c61031b6b93f469d80df61a73ef87d8e6fe
790013438 <790013438@qq.com>
2018-02-14 21:13:32 +0800

    init commit

---
+++ spring-uploading/src/main/java/com/snippets/spring/SpringUploadingApplication.java
@@ -0,0 +1,12 @@
+package com.snippets.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUploadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUploadingApplication.class, args);
+	}
+}
