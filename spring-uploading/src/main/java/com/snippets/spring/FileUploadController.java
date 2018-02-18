c263e2f3e2176e6c1600b9735c5db9dd5959d411
790013438 <790013438@qq.com>
2018-02-18 21:22:34 +0800

    初始提交，不太明白的Autowired
    storageProperties不能Autowired

---
+++ spring-uploading/src/main/java/com/snippets/spring/FileUploadController.java
@@ -0,0 +1,33 @@
+package com.snippets.spring;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+import com.snippets.spring.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+@Controller
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/")
+    public String listUploadedFiles(Model model) throws IOException {
+
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toString())
+                .collect(Collectors.toList()));
+
+        return "uploadForm";
+    }
+}
--- spring-uploading/src/main/java/com/snippets/spring/SpringUploadingApplication.java
+++ spring-uploading/src/main/java/com/snippets/spring/SpringUploadingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringUploadingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringUploadingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUploadingApplication.class, args);
+    }
 }
