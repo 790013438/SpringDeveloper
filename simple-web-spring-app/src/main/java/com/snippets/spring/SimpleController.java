ec5f48431a1c55f6b189162bdfcfeae7c0959c68
790013438 <790013438@users.noreply.github.com>
2018-03-12 14:55:12 +0800

    spring mvc，用注解

--- simple-web-spring-app/src/main/java/com/snippets/spring/SimpleController.java
+++ simple-web-spring-app/src/main/java/com/snippets/spring/SimpleController.java
@@ -1,17 +1,20 @@
 package com.snippets.spring;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+@Controller
+@RequestMapping("/showMessage.html")
+public class SimpleController {
 
-public class SimpleController extends AbstractController {
+    @RequestMapping(method = RequestMethod.GET)
+    protected ModelAndView handleRequestInternal() {
 
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
         ModelAndView modelAndView = new ModelAndView("showMessage");
         modelAndView.addObject("message", "Spring MVC Web Application");
+
         return modelAndView;
     }
 }
