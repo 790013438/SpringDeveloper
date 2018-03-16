465a596dd9c32352103a18649118fb43ac20aa7e
790013438 <790013438@users.noreply.github.com>
2018-03-16 16:24:53 +0800

    SpringBoot web Test, 配置，@Annotation

--- spring-boot-web-test/src/test/java/com/snippets/spring/SpringBootWebTestApplicationTests.java
+++ spring-boot-web-test/src/test/java/com/snippets/spring/SpringBootWebTestApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@SpringApplicationConfiguration(classes = SpringBootWebTestApplication.class)
+@WebAppConfiguration
 public class SpringBootWebTestApplicationTests {
 
     @Test
