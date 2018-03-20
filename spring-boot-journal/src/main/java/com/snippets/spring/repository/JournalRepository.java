76c51d9f4b0ab6d3960f405d30577df857148b8f
790013438 <790013438@users.noreply.github.com>
2018-03-20 14:40:04 +0800

    给REST换名字

--- spring-boot-journal/src/main/java/com/snippets/spring/repository/JournalRepository.java
+++ spring-boot-journal/src/main/java/com/snippets/spring/repository/JournalRepository.java
@@ -3,11 +3,15 @@ package com.snippets.spring.repository;
 import com.snippets.spring.domain.JournalEntryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 
+@Transactional
+@RepositoryRestResource(collectionResourceRel = "entry", path="journal")
 public interface JournalRepository extends JpaRepository<JournalEntryEntity, Long> {
 
     List<JournalEntryEntity> findByCreatedAfter(@Param("after") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)Date date);
