2797dc775bd8bf83a9fee0b08d5c97ba78f425f9
790013438 <790013438@users.noreply.github.com>
2018-03-20 11:29:26 +0800

    RESTful搜索

--- spring-boot-journal/src/main/java/com/snippets/spring/repository/JournalRepository.java
+++ spring-boot-journal/src/main/java/com/snippets/spring/repository/JournalRepository.java
@@ -2,5 +2,18 @@ package com.snippets.spring.repository;
 
 import com.snippets.spring.domain.JournalEntryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 
-public interface JournalRepository extends JpaRepository<JournalEntryEntity, Long> {}
+import java.util.Date;
+import java.util.List;
+
+public interface JournalRepository extends JpaRepository<JournalEntryEntity, Long> {
+
+    List<JournalEntryEntity> findByCreatedAfter(@Param("after") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)Date date);
+    List<JournalEntryEntity> findByCreatedBetween(@Param("after") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)Date after, @Param("before") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date before);
+    List<JournalEntryEntity> findByTitleContaining(@Param("word") String word);
+    List<JournalEntryEntity> findBySummaryContaining(@Param("word") String word);
+
+
+}
