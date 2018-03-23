c0e07aed3748ee6415356afb1d3aa4620ae9551e
790013438 <790013438@users.noreply.github.com>
2018-03-23 13:30:42 +0800

    保护api/**的内容

---
+++ spring-boot-journal-secure/src/main/java/cn/pzhu/spring/config/ResourceSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package cn.pzhu.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalAuthentication
+
+public class ResourceSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/api/**").authenticated()
+            .and()
+            .httpBasic();
+    }
+}
