9888ff16f4498a28d50d49cab7c38a113d6f678d
790013438 <790013438@users.noreply.github.com>
2018-03-27 10:20:27 +0800

    简易方式的JMS，优先使用
    ```SpringBootJmsV2Application
        @JmsListener(destination="${myqueue}")
        public void simplerConsumer(String message) {
            log.info("Simple Consumer> " + message);
        }
    
        @Value("${myqueue}")
        String queue;
    
        @Bean
        CommandLineRunner start(JmsTemplate jmsTemplate) {
            return args -> {
                log.info("Sending> ...");
                jmsTemplate.convertAndSend(queue, "SpringBoot Rocks!");
            };
        }
    ```
    
    ```application.properties
    spring.hornetq.mode=embedded
    spring.hornetq.embedded.enabled=true
    spring.hornetq.embedded.queues=springbootQueue,springQueue
    
    myqueue=springbootQueue
    ```

--- spring-boot-jms-v2/src/main/java/cn/pzhu/spring/SpringBootJmsV2Application.java
+++ spring-boot-jms-v2/src/main/java/cn/pzhu/spring/SpringBootJmsV2Application.java
@@ -1,12 +1,37 @@
 package cn.pzhu.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
 public class SpringBootJmsV2Application {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringBootJmsV2Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJmsV2Application.class, args);
+    }
+
+    @JmsListener(destination="${myqueue}")
+    public void simplerConsumer(String message) {
+        log.info("Simple Consumer> " + message);
+    }
+
+    @Value("${myqueue}")
+    String queue;
+
+    @Bean
+    CommandLineRunner start(JmsTemplate jmsTemplate) {
+        return args -> {
+            log.info("Sending> ...");
+            jmsTemplate.convertAndSend(queue, "SpringBoot Rocks!");
+        };
     }
 }
