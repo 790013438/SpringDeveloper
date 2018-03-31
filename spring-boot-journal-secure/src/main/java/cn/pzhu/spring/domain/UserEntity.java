dcd5abc8929b13f2a97bed8afa20e5cf3620d8cb
790013438 <790013438@qq.com>
2018-03-31 20:35:59 +0800

    创建公共实体类

---
+++ spring-boot-journal-secure/src/main/java/cn/pzhu/spring/domain/UserEntity.java
@@ -0,0 +1,11 @@
+package cn.pzhu.spring.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_user")
+public class UserEntity extends IdEntity{
+
+
+}
