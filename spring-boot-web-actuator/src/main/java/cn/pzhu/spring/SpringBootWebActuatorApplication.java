2717f3f07de240bea6b814e3db17e2470f4df2a0
790013438 <790013438@qq.com>
2018-04-24 09:51:00 +0800

    设置首页,endpoint

--- spring-boot-web-actuator/src/main/java/cn/pzhu/spring/SpringBootWebActuatorApplication.java
+++ spring-boot-web-actuator/src/main/java/cn/pzhu/spring/SpringBootWebActuatorApplication.java
@@ -2,11 +2,19 @@ package cn.pzhu.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootWebActuatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebActuatorApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "Spring Boot Actuator";
     }
 }
