c0e5f2c70798d4859b1ac66e318fd2938f3f49d3
790013438 <790013438@qq.com>
2018-05-08 16:55:42 +0800

    调整试卷的分页和气泡

--- spring-boot-journal-secure/src/main/java/cn/pzhu/spring/web/MySurveyController.java
+++ spring-boot-journal-secure/src/main/java/cn/pzhu/spring/web/MySurveyController.java
@@ -3,13 +3,13 @@ package cn.pzhu.spring.web;
 import cn.pzhu.spring.domain.SurveyEntity;
 import cn.pzhu.spring.repository.SurveyEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @RestController
 public class MySurveyController {
@@ -20,16 +20,17 @@ public class MySurveyController {
     @Autowired
     private SurveyEntityRepository surveyEntityRepository;
 
-    @RequestMapping(value = "/mySurveys", method = RequestMethod.GET)
-    public ModelAndView mySurveys(ModelAndView modelAndView) {
-        List<SurveyEntity> surveyEntityList = surveyEntityRepository.findAll();
-        modelAndView.addObject(VIEW_SURVEY, surveyEntityList);
+    @GetMapping("mySurveys")
+    public ModelAndView mySurveys(ModelAndView modelAndView, @RequestParam(defaultValue = "0") Integer page) {
+        modelAndView.setViewName(VIEW_SURVEY);
+        modelAndView.addObject(VIEW_SURVEY, surveyEntityRepository.findAll(PageRequest.of(page, 4)));
+        modelAndView.addObject("currentPage", page);
         return modelAndView;
     }
 
     @RequestMapping(value = "/mySurveyCreate", method = RequestMethod.GET)
     public ModelAndView mySurveyCreate(ModelAndView modelAndView, @RequestParam("surveyName") String surveyName) {
-        if(surveyName==null || "".equals(surveyName.trim())) {
+        if (surveyName == null || "".equals(surveyName.trim())) {
             surveyName = "请输入问卷标题";
         }
         SurveyEntity surveyEntity = new SurveyEntity();
