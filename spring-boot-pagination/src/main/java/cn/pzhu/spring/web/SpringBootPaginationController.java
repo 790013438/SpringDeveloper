12e9ddbc4655790d999014b16ae095b990eaf381
790013438 <790013438@qq.com>
2018-05-13 15:35:05 +0800

    没有找到SpringBoot使用jsp的方法

--- spring-boot-pagination/src/main/java/cn/pzhu/spring/web/SpringBootPaginationController.java
+++ spring-boot-pagination/src/main/java/cn/pzhu/spring/web/SpringBootPaginationController.java
@@ -1,11 +1,13 @@
 package cn.pzhu.spring.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
+@Controller
 public class SpringBootPaginationController {
 
     private static final String VIEW_INDEX = "index";
