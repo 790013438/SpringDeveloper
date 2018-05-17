cfcc4bffa7eb71ab22ec31e4302987a3ce4a5c6d
790013438 <790013438@qq.com>
2018-05-18 00:19:56 +0800

    init

---
+++ spring-boot-kaptcha/src/main/java/cn/pzhu/spring/SpringBootKaptchaApplication.java
@@ -0,0 +1,12 @@
+package cn.pzhu.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKaptchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKaptchaApplication.class, args);
+    }
+}
