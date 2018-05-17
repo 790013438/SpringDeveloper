8567a69b1661efbc010d6b1a1688308d3d2c7b46
790013438 <790013438@qq.com>
2018-05-18 00:42:56 +0800

    生成图片

---
+++ spring-boot-kaptcha/src/main/java/cn/pzhu/spring/web/SpringBootKaptchaController.java
@@ -0,0 +1,31 @@
+package cn.pzhu.spring.web;
+
+import com.baomidou.kaptcha.Kaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootKaptchaController {
+
+    @Autowired
+    private Kaptcha kaptcha;
+
+    @GetMapping("/render")
+    public void render() {
+        kaptcha.render();
+    }
+
+    @PostMapping("/valid")
+    public void validDefaultTime(@RequestParam String code) {
+        //default timeout 900 seconds
+        kaptcha.validate(code);
+    }
+
+    @PostMapping("/validTime")
+    public void validWithTime(@RequestParam String code) {
+        kaptcha.validate(code, 60);
+    }
+}
