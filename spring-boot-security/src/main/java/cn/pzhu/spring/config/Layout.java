6b39b9d5706e48378e6e2488f8a73557f346f757
790013438 <790013438@qq.com>
2018-05-22 15:27:38 +0800

    thyemleaf,bootstrap,仪表,菜单

---
+++ spring-boot-security/src/main/java/cn/pzhu/spring/config/Layout.java
@@ -0,0 +1,14 @@
+package cn.pzhu.spring.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Layout {
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+      return new LayoutDialect();
+    }
+}
