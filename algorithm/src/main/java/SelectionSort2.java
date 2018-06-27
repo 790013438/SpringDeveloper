08e057cac57a796578360bd48b7aa7acbac6c91c
790013438 <790013438@qq.com>
2018-06-27 10:02:48 +0800

    修改有bug的版本,逻辑问题引发下标出界

---
+++ algorithm/src/main/java/SelectionSort2.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class SelectionSort2 {
+
+    // this version uses raw arrays instead of ArrayList
+    private static int[] selectionSort(int[] arr) {
+        int[] newArr = new int[arr.length];
+
+        for (int i = 0; i < newArr.length; ++i) {
+            int smallestIndex = findSamllest(arr);
+            newArr[i] = arr[smallestIndex];
+
+            arr = getNewArrWithoutSamllest(arr, smallestIndex);
+        }
+
+        return newArr;
+    }
+
+    private static int[] getNewArrWithoutSamllest(int[] arr, int smallestIndex) {
+        int[] newArrayWithoutSmallest = new int[arr.length - 1];
+        for (int i = 0; i < arr.length && smallestIndex != 0; ++i) {
+            if (i < smallestIndex) {
+                newArrayWithoutSmallest[i] = arr[i];
+            } else {
+                newArrayWithoutSmallest[i - 1] = arr[i];
+            }
+        }
+        return newArrayWithoutSmallest;
+    }
+
+    private static int findSamllest(int[] arr) {
+        int smallest = arr[0];
+        int smallestIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 6, 2, 10};
+        System.out.println(Arrays.toString(selectionSort(arr)));
+    }
+}
